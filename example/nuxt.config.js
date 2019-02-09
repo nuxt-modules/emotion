6c8ab3e5a0a36e2306070355f16c3956583765c1
pooya parsa <pooya@pi0.ir>
2019-02-09 23:47:58 +0330

    test: fix ci builds

--- example/nuxt.config.js
+++ example/nuxt.config.js
@@ -4,6 +4,12 @@ module.exports = {
   rootDir: resolve(__dirname, '..'),
   buildDIr: resolve(__dirname, '.nuxt'),
   srcDir: __dirname,
+  build: {
+    filenames: {
+      app: '[name].js',
+      chunk: '[name].js'
+    }
+  },
   render: {
     resourceHints: false
   },
