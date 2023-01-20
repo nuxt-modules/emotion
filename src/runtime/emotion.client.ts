da7d46fa94738c1b94e45b7b805f7f5fc1f235ca
codebender828 <jbakebwa@163.com>
2023-01-21 02:03:17 +0800

    fix: linting problems

--- src/runtime/emotion.client.ts
+++ src/runtime/emotion.client.ts
@@ -1,9 +1,9 @@
-import { hydrate } from "@emotion/css";
-import { defineNuxtPlugin } from "#app";
+import { hydrate } from '@emotion/css'
+import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((_) => {
   if (window.$emotionSSRIds) {
-    const ids = window.$emotionSSRIds;
-    hydrate(ids);
+    const ids = window.$emotionSSRIds
+    hydrate(ids)
   }
-});
+})
