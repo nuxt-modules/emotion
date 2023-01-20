d723e80c295ccd15f250b0b887f20fad1b968804
codebender828 <jbakebwa@163.com>
2023-01-21 02:06:25 +0800

    chore: update snapshots

--- src/module.ts
+++ src/module.ts
@@ -1,10 +1,10 @@
-import { fileURLToPath } from "url";
+import { fileURLToPath } from 'url'
 import {
   defineNuxtModule,
   addPlugin,
   createResolver,
-  addServerPlugin,
-} from "@nuxt/kit";
+  addServerPlugin
+} from '@nuxt/kit'
 
 declare global {
   interface Window {
@@ -15,31 +15,31 @@ declare global {
 
 export default defineNuxtModule({
   meta: {
-    name: "@nuxtjs/emotion",
-    configKey: "emotion",
-    compatibilty: ">=3.0.0",
+    name: '@nuxtjs/emotion',
+    configKey: 'emotion',
+    compatibilty: '>=3.0.0'
   },
-  setup(_, nuxt) {
+  setup (_, nuxt) {
     // ensure `nitro.plugins` is initialized
-    nuxt.options.nitro.plugins = nuxt.options.nitro.plugins || [];
+    nuxt.options.nitro.plugins = nuxt.options.nitro.plugins || []
 
-    nuxt.hook("nitro:config", (config) => {
+    nuxt.hook('nitro:config', (config) => {
       // Prevent inlining emotion (+ the crucial css cache!) in dev mode
       if (nuxt.options.dev) {
         if (config.externals) {
-          config.externals.external ||= [];
-          config.externals.external.push("@emotion/server");
+          config.externals.external ||= []
+          config.externals.external.push('@emotion/server')
         }
       }
-    });
+    })
 
     /**
      * Register emotion plugin
      */
-    const { resolve } = createResolver(import.meta.url);
-    const runtimeDir = fileURLToPath(new URL("./runtime", import.meta.url));
-    nuxt.options.build.transpile.push(runtimeDir);
-    addServerPlugin(resolve(runtimeDir, "emotion.server"));
-    addPlugin(resolve(runtimeDir, "emotion.client"));
-  },
-});
+    const { resolve } = createResolver(import.meta.url)
+    const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
+    nuxt.options.build.transpile.push(runtimeDir)
+    addServerPlugin(resolve(runtimeDir, 'emotion.server'))
+    addPlugin(resolve(runtimeDir, 'emotion.client'))
+  }
+})
