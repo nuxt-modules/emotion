fdb10ad617aaaba2b4d23493eb75778aee674cfa
codebender828 <jbakebwa@163.com>
2023-01-21 18:00:47 +0800

    test: updtae test for ssrIds

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -9,7 +9,7 @@ describe('nust emotion ssr', async () => {
     const html = await $fetch('/')
     expect(html).toContain('<style data-emotion')
     expect(html).toContain('<script data-emotion')
-    expect(html).toContain('window.$emotionIds')
+    expect(html).toContain('window.$emotionSSRIds')
 
     // Removed because the nuxt build entries product difference SSR output
     // for each test run so it doesn't make sense to run snapshot tests.
