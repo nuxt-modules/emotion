a99a648697d40f808cd53f27da42c5f637cc4e59
codebender828 <jbakebwa@163.com>
2023-01-21 17:42:01 +0800

    fix: ssr ids should be the same

--- src/runtime/emotion.server.ts
+++ src/runtime/emotion.server.ts
@@ -1,5 +1,5 @@
-import { extractCritical } from '@emotion/server'
-import { NitroApp } from 'nitropack'
+import { extractCritical } from "@emotion/server";
+import { NitroApp } from "nitropack";
 
 /**
  * Why are we declaring types for  `defineNitroPlugin`?
@@ -20,18 +20,18 @@ export interface NitroAppPlugin {
   (nitro: NitroApp): void;
 }
 
-export function defineNitroPlugin (def: NitroAppPlugin): NitroAppPlugin {
-  return def
+export function defineNitroPlugin(def: NitroAppPlugin): NitroAppPlugin {
+  return def;
 }
 
 export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('render:html', (html) => {
-    const { ids, css } = extractCritical(html.body)
-    html.head.push(`<style data-emotion="${ids.join(' ')}">${css}</style>`)
+  nitroApp.hooks.hook("render:html", (html) => {
+    const { ids, css } = extractCritical(html.body);
+    html.head.push(`<style data-emotion="${ids.join(" ")}">${css}</style>`);
     html.head.push(
       `<script data-emotion="${ids.join(
-        ' '
-      )}">window.$emotionIds=${JSON.stringify(ids)}</script>`
-    )
-  })
-})
+        " "
+      )}">window.$emotionSSRIds=${JSON.stringify(ids)}</script>`
+    );
+  });
+});
