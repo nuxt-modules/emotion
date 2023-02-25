999700a52c0aee20b3d9785e0050b6d5b3b1d3c9
codebender828 <jbakebwa@163.com>
2023-02-25 15:16:39 +0800

    chore: fix lint

--- src/module.ts
+++ src/module.ts
@@ -2,8 +2,8 @@ import {
   defineNuxtModule,
   addPlugin,
   createResolver,
-  addServerPlugin,
-} from "@nuxt/kit";
+  addServerPlugin
+} from '@nuxt/kit'
 
 declare global {
   interface Window {
@@ -14,31 +14,31 @@ declare global {
 
 export default defineNuxtModule({
   meta: {
-    name: "@nuxtjs/emotion",
-    configKey: "emotion",
-    compatibilty: ">=3.0.0",
+    name: '@nuxtjs/emotion',
+    configKey: 'emotion',
+    compatibilty: '>=3.0.0'
   },
-  setup(_, nuxt) {
+  setup (_, nuxt) {
     // ensure `nitro.plugins` is initialized
-    nuxt.options.nitro.plugins = nuxt.options.nitro.plugins || [];
+    nuxt.options.nitro.plugins = nuxt.options.nitro.plugins || []
 
-    nuxt.hook("nitro:config", (config) => {
+    nuxt.hook('nitro:config', (config) => {
       // Prevent inlining emotion (+ the crucial css cache!) in dev mode
       if (nuxt.options.dev) {
         if (config.externals) {
-          config.externals.external ||= [];
-          config.externals.external.push("@emotion/server");
+          config.externals.external ||= []
+          config.externals.external.push('@emotion/server')
         }
       }
-    });
+    })
 
     /**
      * Register emotion plugin
      */
-    const { resolve } = createResolver(import.meta.url);
-    const runtimeDir = resolve("./runtime");
-    nuxt.options.build.transpile.push(runtimeDir);
-    addServerPlugin(resolve(runtimeDir, "emotion.server"));
-    addPlugin(resolve(runtimeDir, "emotion.client"));
-  },
-});
+    const { resolve } = createResolver(import.meta.url)
+    const runtimeDir = resolve('./runtime')
+    nuxt.options.build.transpile.push(runtimeDir)
+    addServerPlugin(resolve(runtimeDir, 'emotion.server'))
+    addPlugin(resolve(runtimeDir, 'emotion.client'))
+  }
+})
